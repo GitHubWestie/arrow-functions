f254121ddca0db03f7e0f75751c2677c11ff8481
Keiran <127797224+GitHubWestie@users.noreply.github.com>
2024-02-28 17:54:22 +0000

    added code through following lesson

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers = (a, b) => a + b;
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Well, hello');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMulitpleLines = () => (
+    `<p>
+    This is a multi line return
+    </p>`
+)
+console.log(returnMulitpleLines());
